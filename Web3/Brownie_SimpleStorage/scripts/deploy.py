798ffdb4e416f366b1e2f8224789684fcb0fcc8e
C-Mierez <carlos.mierez20@gmail.com>
2022-02-03 19:06:34 -0300

    Change account selection

--- Web3/Brownie_SimpleStorage/scripts/deploy.py
+++ Web3/Brownie_SimpleStorage/scripts/deploy.py
@@ -1,29 +1,29 @@
 # Easier way to work with accounts
 import re
-from brownie import accounts, config, SimpleStorage
+from brownie import accounts, config, SimpleStorage, network
 
 import os
 
 def deploy_simple_storage():
     # Grab the first account from the generated accounts (Ganache)
     # Only works with local networks
-    localAccount = accounts[0]
+    # localAccount = accounts[0]
     
     # In order to work with an actual account for an actual network
     # We can add the address to the Brownie accounts list from the cmd line
     # Then it can be accessed like this:
-    realAccount = accounts.load("cmdev")
+    # realAccount = accounts.load("cmdev")
     
     # Or just like with the non-brownie way
     # Environemnt variables in the .env file can be used
-    envAccount = accounts.add(os.getenv("PRIVATE_KEY"))
+    # envAccount = accounts.add(os.getenv("PRIVATE_KEY"))
     
     # An even better option is to set and fetch the account from the .yaml config file
-    configAccount = accounts.add(config["wallets"]["from_key"])
+    # configAccount = accounts.add(config["wallets"]["from_key"])
     
     #? I leave all the above in the code for demonstration purposes
     #? [account] is the one that's going to be used throughout this project
-    account = localAccount
+    account = get_account()
     
     # Deploying the contract
     contract_sp = SimpleStorage.deploy({"from": account})
@@ -41,6 +41,14 @@ def deploy_simple_storage():
     # Updated retrieve() value
     stored_value = contract_sp.retrieve()
     print(f"Updated stored value: {stored_value}.")
+
+def get_account():    
+    if network.show_active() == "development":
+        # Using Ganache local account
+        account = accounts[0]
+    else:
+        account = accounts.add(config["wallets"]["from_key"])
+    return account
 
 def main():
     deploy_simple_storage()
