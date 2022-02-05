afb3ae3e518a4f53fb7b5392a66c33365037d390
C-Mierez <carlos.mierez20@gmail.com>
2022-02-05 15:49:24 -0300

    Dynamic network deployment

--- Web3/Brownie_FundMe/scripts/deploy.py
+++ Web3/Brownie_FundMe/scripts/deploy.py
@@ -1,11 +1,36 @@
-from brownie import FundMe
+from brownie import FundMe, network, config, MockV3Aggregator
 
 from scripts.utils import get_account
 
+
 def deploy_fund_me():
     account = get_account()
-    contract_fm = FundMe.deploy({"from": account}, publish_source=True)
+    # After the changes in the contract's constructor, it now needs an
+    # additional parameter (Rinkeby Chainlink contract address)
+    # This allows dynamically changing the used address depending on the env (Test, Local, Live,...)
+    active_network = network.show_active()
+    if active_network != "development":
+        priceFeed_address = config["networks"][active_network]["eth_usd_priceFeed"]
+    else:
+        # If in development, deploy a mock
+        print(
+            f"Active network: {active_network}.\nDeploying mock priceFeed contract..."
+        )
+        contract_mock_V3Agg = MockV3Aggregator.deploy(
+            18,
+            2000000000000000000000,
+            {"from": account},
+        )
+        priceFeed_address = contract_mock_V3Agg.address
+        print("Mock priceFeed contract deployed.")
+
+    contract_fm = FundMe.deploy(
+        priceFeed_address,
+        {"from": account},
+        publish_source=config["networks"][active_network]["verify"],
+    )
     print(f"Contract deployed to: {contract_fm.address}")
+
 
 def main():
     deploy_fund_me()
--- Web3/Brownie_FundMe/scripts/utils.py
+++ Web3/Brownie_FundMe/scripts/utils.py
@@ -1,8 +1,8 @@
 from brownie import accounts, network, config
 
+
 def get_account():
-    if network.show_active == "development":
+    if network.show_active() == "development":
         return accounts[0]
     else:
-        return accounts.add(config["wallets"]["from_key"]) 
-    
+        return accounts.add(config["wallets"]["from_key"])
