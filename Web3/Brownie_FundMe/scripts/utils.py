f4292f83e0da52c16d6614885deb28dc9cb96a17
C-Mierez <carlos.mierez20@gmail.com>
2022-02-05 15:59:47 -0300

    Modularization of the mock contract deployment

--- Web3/Brownie_FundMe/scripts/utils.py
+++ Web3/Brownie_FundMe/scripts/utils.py
@@ -1,4 +1,10 @@
-from brownie import accounts, network, config
+from brownie import accounts, network, config, MockV3Aggregator
+
+from web3 import Web3
+
+# priceFeed Mock Parameters
+DECIMALS = 18
+STARTING_PRICE = 2000
 
 
 def get_account():
@@ -6,3 +12,15 @@ def get_account():
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
+
+
+def deploy_mock_priceFeed():
+    if len(MockV3Aggregator) <= 0:
+        MockV3Aggregator.deploy(
+            DECIMALS,
+            Web3.toWei(STARTING_PRICE, "ether"),
+            {"from": get_account()},
+        )
+        print("Mock priceFeed contract deployed.")
+    else:
+        print("Mock priceFeed contract already deployed.")
