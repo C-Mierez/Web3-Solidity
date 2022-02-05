6fe8d09967d301f50936d581d5b95ec244e4b033
C-Mierez <carlos.mierez20@gmail.com>
2022-02-05 13:24:56 -0300

    Set up dependencies

---
+++ Web3/Brownie_FundMe/contracts/FundMe.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+
+contract FundMe {
+    using SafeMathChainlink for uint256;
+
+    address owner;
+
+    // Store the address of the funder 
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+    
+    constructor() public {
+        owner = msg.sender;     
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only owner.");
+        _;
+    }
+
+    function fund() public payable {
+        // Set a threshold for funding in terms of USD value
+        uint256 minimumUSD = 50 * (10 ** 18);
+        require(getConversionRate(msg.value) >= minimumUSD, "More ETH required.");
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+    }
+
+    function getVersion() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
+        return priceFeed.version();
+    }
+
+    function getPrice() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
+        (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        ) = priceFeed.latestRoundData();
+        return uint256(answer);
+    }
+
+    // 1 Eth = 1000000000 Gwei
+    function getConversionRate(uint256 _ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = _ethAmount * ethPrice / (10 ** 8);
+        return ethAmountInUsd;
+    }
+
+    function withdraw() public payable onlyOwner {
+        msg.sender.transfer(address(this).balance);
+        for (uint256 i = 0; i < funders.length; i++){
+            address funder = funders[i];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+    }
+
+}
