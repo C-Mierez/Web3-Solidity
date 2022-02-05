87b35eddb95890dc1a5c9199d55d8ac5d0e15afb
C-Mierez <carlos.mierez20@gmail.com>
2022-02-05 19:07:24 -0300

    Added forked mainnet support

--- Web3/Brownie_FundMe/scripts/utils.py
+++ Web3/Brownie_FundMe/scripts/utils.py
@@ -7,10 +7,14 @@ STARTING_PRICE = 200000000000
 
 # Environment definitions
 LOCAL_BLOCKCHAIN_ENV = ["development", "ganache-local"]
+FORKED_BLOCKCHAIN_ENV = ["mainnet-fork", "mainnet-fork-dev"]
 
 
 def get_account():
-    if network.show_active() in LOCAL_BLOCKCHAIN_ENV:
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENV
+        or network.show_active() in FORKED_BLOCKCHAIN_ENV
+    ):
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
