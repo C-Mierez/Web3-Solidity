d9dbe737f6c13fd3244774f8b1e2900855ad607f
C-Mierez <carlos.mierez20@gmail.com>
2022-02-06 02:25:22 -0300

    Finished adding retrieving needed params

--- Web3/Brownie_Lottery/scripts/deploy_lottery.py
+++ Web3/Brownie_Lottery/scripts/deploy_lottery.py
@@ -1,4 +1,4 @@
-from brownie import accounts, Lottery
+from brownie import accounts, Lottery, config, network
 
 from scripts.utils import get_account, get_contract
 
@@ -6,9 +6,15 @@ from scripts.utils import get_account, get_contract
 def deploy_lottery():
     account = get_account(id="cmdev")
     contract_l = Lottery.deploy(
-        get_contract("eth_usd_price_feed"),
+        get_contract("eth_usd_price_feed").address,
+        get_contract("vrf_coordinator").address,
+        get_contract("link_token").address,
+        config["networks"][network.show_active()]["fee"],
+        config["networks"][network.show_active()]["keyhash"],
         {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify", False),
     )
+    print("Deployed.")
 
 
 def main():
