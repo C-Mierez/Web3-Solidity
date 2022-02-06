76b59372e725ff70ef21d67b4c5bee4b8ef34ff6
C-Mierez <carlos.mierez20@gmail.com>
2022-02-06 17:20:02 -0300

    Added all the contract functions

--- Web3/Brownie_Lottery/scripts/deploy_lottery.py
+++ Web3/Brownie_Lottery/scripts/deploy_lottery.py
@@ -1,11 +1,12 @@
 from brownie import accounts, Lottery, config, network
 
-from scripts.utils import get_account, get_contract
+from scripts.utils import get_account, get_contract, fund_with_link
 
 
 def deploy_lottery():
     account = get_account(id="cmdev")
-    contract_l = Lottery.deploy(
+    print("Deploying contracts...")
+    Lottery.deploy(
         get_contract("eth_usd_price_feed").address,
         get_contract("vrf_coordinator").address,
         get_contract("link_token").address,
@@ -17,5 +18,42 @@ def deploy_lottery():
     print("Deployed.")
 
 
+def start_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    print("Starting lottery...")
+    tx = lottery.startLottery({"from": account})
+    tx.wait(1)
+    print("Lottery started.")
+
+
+def enter_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+
+    print("Entering lottery...")
+    # Entrance Fee
+    value = lottery.getEntranceFee() + (10**7)
+    tx = lottery.bid({"from": account, "value": value})
+    tx.wait(1)
+    print("Entered lottery.")
+
+
+def end_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+
+    tx = fund_with_link(lottery.address, account)
+    tx.wait(1)
+
+    print("Ending lottery...")
+    tx = lottery.endLottery({"from": account})
+    tx = tx.wait(1)
+    print("Ended lottery.")
+
+
 def main():
     deploy_lottery()
+    start_lottery()
+    enter_lottery()
+    end_lottery()
