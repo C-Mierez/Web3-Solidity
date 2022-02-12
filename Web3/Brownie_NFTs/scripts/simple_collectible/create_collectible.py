417182ce2474e2cc1146a5ab8fcb93e6f905d7b4
C-Mierez <carlos.mierez20@gmail.com>
2022-02-12 17:07:34 -0300

    Clean up

--- Web3/Brownie_NFTs/scripts/simple_collectible/create_collectible.py
+++ Web3/Brownie_NFTs/scripts/simple_collectible/create_collectible.py
@@ -5,7 +5,7 @@ sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6
 OPENSEA_URL = "https://testnets.opensea.io/assets/{}/{}"
 
 
-def main():
+def deploy_and_create():
     account = get_account()
 
     simple_collectible = SimpleCollectible.deploy({"from": account})
@@ -18,3 +18,8 @@ def main():
     print(
         f"SimpleCollectible created. OpenSea: {OPENSEA_URL.format(simple_collectible.address, simple_collectible.tokenCounter() - 1)}"
     )
+    return simple_collectible
+
+
+def main():
+    deploy_and_create()
