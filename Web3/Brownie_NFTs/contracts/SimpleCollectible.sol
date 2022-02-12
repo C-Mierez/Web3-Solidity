f4c430323865b01f82f8bac19f1316292e51e8e0
C-Mierez <carlos.mierez20@gmail.com>
2022-02-12 15:56:07 -0300

    Initial ERC721 token template

---
+++ Web3/Brownie_NFTs/contracts/SimpleCollectible.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract SimpleCollectible is ERC721 {
+    // Amount of tokens minted. Used as an incremental unique id
+    uint256 public tokenCounter;
+
+    constructor() public ERC721("SimpleStonks", "STONK") {
+        tokenCounter = 0;
+    }
+
+    function createCollectible(string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        uint256 newTokenId = tokenCounter;
+
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        tokenCounter += 1;
+        return newTokenId;
+    }
+}
